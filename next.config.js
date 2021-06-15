596263ac344b6fc2fe030629439d76cebb03c672
jonathanprozzi <jonathanprozzi@gmail.com>
2021-06-15 01:13:35 -0400

    updated config for fleek

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
 const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
 module.exports = withNextra()
+
+module.exports = {
+  assetPrefix: './',
+  trailingSlash: true,
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+    }
+  },
+}
